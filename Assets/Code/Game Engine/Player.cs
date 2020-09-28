9bd7d2ea2a4df1224a243d473d041c7a499784a7
westonjones <70167570+westonjones@users.noreply.github.com>
2020-09-28 08:56:06 -0500

    Game engine 1st implementation code
    Added Room, Player, and Game Engine. First iteration, some functions missing, many functions just shells.

---
+++ Assets/Code/Game Engine/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public bool myTurn;
+    public int goldReserve;
+    public int goldIncome;
+    public int mushroomReserve;
+    public int mushroomIncome;
+    public int mushroomUpkeep;
+    public int actions;
+    public List<Room> ownedRooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public Player()
+    {
+        myTurn = false;
+        goldReserve = 10;
+        goldIncome = 0;
+        mushroomReserve = 0;
+        mushroomIncome = 6;
+        mushroomUpkeep = 6;
+        actions = 0;
+    }
+
+    public void StartTurn()
+    {
+        actions = 2;
+        myTurn = true;
+        CalcIncome();
+        MushroomEconomy();
+        goldReserve += goldIncome;
+
+    }
+
+    void MushroomEconomy()
+    {
+        int rem = mushroomIncome - mushroomUpkeep;
+        if (rem > 0)
+        {
+            mushroomReserve += rem;
+        }
+        else if (rem < 0)
+        {
+            print("Error: Remainder in playerMushroomEconomy less than 0, should not happen.");
+        }
+    }
+
+    void UsedAction()
+    {
+        actions--;
+        if (actions == 0)
+        {
+            //endTurn();
+        }
+    }
+
+    void EndTurn()
+    {
+
+    }
+
+    void OwnRoom(Room newOwned)
+    {
+        ownedRooms.Add(newOwned);
+    }
+
+    void CalcIncome()
+    {
+        int newMushroomIncome = 0;
+        int newGoldIncome = 0;
+        foreach (var thisRoom in ownedRooms)
+        {
+            newMushroomIncome += thisRoom.GetMushroomIncome();
+            newGoldIncome += thisRoom.GetGoldIncome();
+        }
+        mushroomIncome = newMushroomIncome;
+        goldIncome = newGoldIncome; 
+    }
+
+
+
+
+
+}

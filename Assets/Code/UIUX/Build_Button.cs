640df7fe58586dad8752b454698f5e41027d8fe8
westonjones <70167570+westonjones@users.noreply.github.com>
2020-09-30 08:48:58 -0500

    Interactions and Prefabs
    Created UI scripts: Build_Button, MoveUnit_Button
    Implemented ability to select room and select unit.
    Selecting a room will highlight it via the Selection Light moving to its position (y+lightHeight)
    Added first iteration as GameEngine functions: Build and Move Unit
    Build: Only builds 1 selection right now
    Move: Moves unit regardless of any rules logic to selected room.
    
    Attempted to hook up UI buttons Build and Move to respected functions, do not appear responsive?
    
    Created proxy prefabs for buildings and dwarf unit

---
+++ Assets/Code/UIUX/Build_Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Build_Button : MonoBehaviour
+{
+    public GameObject gameEngine;
+    public Button myButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton.onClick.AddListener(Build);
+    }
+
+    private void Build()
+    {
+        print("Build button hit");
+        gameEngine.GetComponent<GameEngine>().Build(4); //Forces choice 4 for testing purposes, needs to be changed.
+    }
+}
---
+++ Assets/Code/UIUX/MoveUnit_Button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveUnit_Button : MonoBehaviour
+{
+    public GameObject gameEngine;
+    public GameObject myself;
+    public Button myButton; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        myButton.onClick.AddListener(MoveUnit);
+    }
+
+    private void MoveUnit()
+    {
+        print("Move button hit");
+        gameEngine.GetComponent<GameEngine>().MoveUnit();
+    }
+}

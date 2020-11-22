f864e424e1eb1810f992932d0caf998db8ea887d
Yuno <yunominovo@gmail.com>
2020-11-22 14:31:26 -0600

    fix bug in tooltips

--- Assets/Code/UIUX/TooltipTrigger.cs
+++ Assets/Code/UIUX/TooltipTrigger.cs
@@ -7,12 +7,20 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 {
     [Multiline()]
     public string content;
+
+    //private static LTDescr delay;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
+        //delay = LeanTween.delayedCall(0.5f, () =>
+        //{
+        //    TooltipSystem.Show(content);
+        //});
         TooltipSystem.Show(content);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
+        //LeanTween.cancel(delay.uniqueId);
         TooltipSystem.Hide();
     }
 }
